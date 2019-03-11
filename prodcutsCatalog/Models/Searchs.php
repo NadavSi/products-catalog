6dbfb3cad5f92899624756cfb5f68ae2db02bde5
NadavSi <nadav.siv@gmail.com>
2019-03-11 23:38:38 +0200

    Add files via upload

--- prodcutsCatalog/Models/Searchs.php
+++ prodcutsCatalog/Models/Searchs.php
@@ -7,15 +7,30 @@
     }
 
     /*
+    * function checks if log table exist and if not creates it
+    * return boolean
+    */
+    public function logTableExist(){
+        $this->db->query("SELECT * FROM information_schema.tables WHERE table_schema = '" . DB_NAME . "' AND table_name = 'searchlog' LIMIT 1");
+        // Execute
+        $this->db->execute();
+        if($this->db->rowCount() > 0) {
+          return true;
+        } else {
+          return $this->createLogTable();
+        }
+    }
+
+    /*
     * function get all log entries from db
     * return array
     */
     public function getSearchLog(){
-      $this->db->query('SELECT * FROM `searchlog`');
+        $this->db->query('SELECT * FROM `searchlog`');
 
-      $results = $this->db->resultSet();
+        $results = $this->db->resultSet();
 
-      return $results;
+        return $results;
     }
 
     /*
@@ -24,17 +39,35 @@
     * $searchCriteria - search criteria selected by user
     */
     public function addSearchLog($searchStr,$searchCriteria){
-      $this->db->query('INSERT INTO `searchlog` (search_string, search_criteria, user_ip) VALUES(:searchStr, :searchCri, :user_ip)');
-      // Bind values
-      $this->db->bind(':searchStr', $searchStr);
-      $this->db->bind(':searchCri', $searchCriteria);
-      $this->db->bind(':user_ip', $_SERVER['SERVER_ADDR']);
+        $this->db->query('INSERT INTO `searchlog` (search_string, search_criteria, user_ip) VALUES(:searchStr, :searchCri, :user_ip)');
+        // Bind values
+        $this->db->bind(':searchStr', $searchStr);
+        $this->db->bind(':searchCri', $searchCriteria);
+        $this->db->bind(':user_ip', $_SERVER['SERVER_ADDR']);
 
-      // Execute
-      if($this->db->execute()){
-        return true;
+        // Execute
+        if($this->db->execute()){
+          return true;
       } else {
-        return false;
+          return false;
       }
+    }
+
+    private function createLogTable(){
+        $sqlStr = "CREATE TABLE `searchlog` (";
+        $sqlStr .= "`id` int(11) NOT NULL AUTO_INCREMENT,";
+        $sqlStr .=  "`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,";
+        $sqlStr .=  "`search_string` varchar(100) NOT NULL,";
+        $sqlStr .= "`search_criteria` varchar(10) NOT NULL,";
+        $sqlStr .=  "`user_ip` varchar(20) NOT NULL,";
+        $sqlStr .=  "PRIMARY KEY (`id`))";
+        $this->db->query($sqlStr);
+
+        // Execute
+        if($this->db->execute()){
+            return true;
+        } else {
+            return false;
+        }
     }
   }
