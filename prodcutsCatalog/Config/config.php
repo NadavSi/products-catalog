e4b4d66405d0b1d45b49e7452ba242661b716559
NadavSi <nadav.siv@gmail.com>
2019-03-15 16:37:55 +0200

    Update config.php

--- prodcutsCatalog/Config/config.php
+++ prodcutsCatalog/Config/config.php
@@ -2,8 +2,8 @@
   // DB Params
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
-  define('DB_PASS', '123456');
-  define('DB_NAME', 'ushops');
+  define('DB_PASS', '');
+  define('DB_NAME', '');
 
   // Site Name
   define('SITENAME', 'Products Catalog');
